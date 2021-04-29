377b4fb7d4a01a0de2424c671ae4e83b2802ff04
Nivan Jr <78574377+nivslive@users.noreply.github.com>
2021-04-29 19:18:22 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const target = document.querySelectorAll ('[data-anime]');
+
+const animationClass = 'animate';
+
+animeScroll();
+
+function animeScroll() {
+const windowTop = window.pageYOffset + ((window.innerHeight * 3 / 4));
+console.log(windowTop);
+target.forEach(function(element){
+
+
+	if ((windowTop) > element.offsetTop){
+	console.log(element.classList.add(animationClass));
+}
+})
+
+
+
+}
+
+
+window.addEventListener('scroll', function(){
+animeScroll();
+
+
+})
+	
+
+target.forEach(item=> {
+
+console.log(item);
+
+})
